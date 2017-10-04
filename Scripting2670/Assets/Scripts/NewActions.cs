8ff25b2cfae9c9fd869ddf3555cba58e06bd3006
Anthony Romrell <anthonyromrell@mac.com>
2017-10-04 14:18:57 -0600

    UnityActions

---
+++ Scripting2670/Assets/Scripts/NewActions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NewActions : MonoBehaviour {
+
+	public UnityAction ControlCharacter;
+
+	void Start () {
+		ControlCharacter = Idle;
+	}
+
+	void OnMouseDown()
+	{
+		ControlCharacter = Walk;
+	}
+
+	void OnMouseUp()
+	{
+		ControlCharacter = Walk;
+	}
+
+    private void Idle()
+    {
+        print("Idling");
+    }
+
+	private void Walk()
+    {
+        print("Walking");
+    }
+	private void Jump()
+    
+	{
+        print("Jumping");
+    }
+
+	void Update()
+	{
+		ControlCharacter();	
+	}
+}
