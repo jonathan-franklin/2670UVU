6493cb767b13ddf6ef4980fce477f1e3cfa4f351
Anthony Romrell <anthonyromrell@mac.com>
2017-10-06 13:16:58 -0600

    Houdini Asset Tree

---
+++ New Unity Project/Assets/testing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Houdini;
+
+public class testing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print (this.GetComponent<HoudiniApiAssetAccessorExample> ().parmIndex);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
