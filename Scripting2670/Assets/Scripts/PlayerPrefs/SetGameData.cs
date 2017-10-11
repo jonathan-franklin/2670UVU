55db1f11f012a4dbc709557c5504ff40ad9a9559
Anthony Romrell <anthonyromrell@mac.com>
2017-10-11 16:12:22 -0600

    Player Prefs

---
+++ Scripting2670/Assets/Scripts/PlayerPrefs/SetGameData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SetGameData : MonoBehaviour {
+
+	public Data data;
+
+	void OnApplicationQuit()
+	{
+		data.checkPoint = transform.localPosition;
+		PlayerPrefs.SetString("GameData", JsonUtility.ToJson(data));
+	}
+}
