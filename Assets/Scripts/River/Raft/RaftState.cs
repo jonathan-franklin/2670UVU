141a82c4c5583a70c416290d7649419bb5055e4f
Jonathan Franklin <jonathan.t.franklin@gmail.com>
2017-10-13 15:09:29 -0600

    Auto stash before merge of "LearningAssets" and "anthonyromrell/LearningAssets"

--- Assets/Scripts/River/Raft/RaftState.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RaftState : MonoBehaviour 
-{
-    public static System.Action<bool,bool> SendRaftStatus;
-
-}
